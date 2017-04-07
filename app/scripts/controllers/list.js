e9d53c5425ea636c5be5fd2c0caf45b97eb93cf1
bindupothana <himabindu.pothana@navaratan.com>
2017-04-07 17:27:58 +0530

    Rtf application

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -1,9 +1,7 @@
  var app = angular.module('rtxDemoApp');
   app.controller('listCtrl',['$scope','$stateParams','$state','questionFactory',
   	function($scope,$stateParams,$state, questionFactory) {
-    // $scope.go_to_new_emp=function(user){
-    // $state.go("newemployee");
-    // };
+
      $scope.submitNewEmployee=function(){
      $state.go("list");
      };
@@ -17,33 +15,7 @@
       }
         
      	$scope.getQuestion(); 
-     	 $scope.selectedAnswerForQues = function(key, inner_key){
-        if($scope.selectedAns.length == 0){
-          var answer = {
-            question: key ,
-            userSelectedAnswer: inner_key
-          }
-          $scope.selectedAns.push(answer);
-        }
-       else {
-          var found = false;
-          for(var i=0; i< $scope.selectedAns.length; i++){
-            if($scope.selectedAns[i].question == key){
-             found = true;
-             $scope.selectedAns[i].userSelectedAnswer = inner_key;
-           }
-         }
-         if(!found){
-          var answer = {
-            question: key,
-            userSelectedAnswer: inner_key
-          }
-          $scope.selectedAns.push(answer);
-        } 
-      }
-      console.log("Ans",$scope.selectedAns);
-      console.log("key, inner_key", key, inner_key)
-    }
+     	 
 
    
 
