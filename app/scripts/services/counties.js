a2883399fa5404aa0f0c39dc926a0bbf5b3505c5
bindupothana <himabindu.pothana@navaratan.com>
2017-04-14 15:45:23 +0530

    rtx_demo

--- app/scripts/services/counties.js
+++ app/scripts/services/counties.js
@@ -1,21 +1,25 @@
+
+
+
+
 var app = angular.module('rtxDemoApp');
 
-app.service('countyService', ['$http',function($http) {   
-  return{
-       getCounty:function(counties) {
-       console.log('call http with object', counties);
-       return  $http({
+app.service('countyService', ['$http',function($http) { 
+   return{
+        getCounty:function(counties) {
+           var user_info = JSON.parse(localStorage.getItem('user_info'));
+              // console.log("service_user_info",user_info)
+           var auth_info = user_info.auth_token;
+               console.log("ksjdfgkhg",auth_info)
+               console.log('call http with object', counties);
+    return  $http({
                 method: 'GET',
-                url: 'https://api-qa.retrotax-aci.com/states/{state_code}/counties'    
-               
-                headers: { "X-API-KEY": "yqvNrVR5Cs6vhLq1ZRPq38GM5OrXJ7C97n4BZCJa", "Content-Type": "application/json"},
+              url: 'https://api-qa.retrotax-aci.com/states/{state_code}/counties',   
+                data: counties,
+                headers: { "X-API-KEY": "yqvNrVR5Cs6vhLq1ZRPq38GM5OrXJ7C97n4BZCJa", "X-AUTH-TOKEN": "auth_info","Content-Type": "application/json"},
                 responseType: 'json'
-              });
-          }
-        }
+                });
+            }
+     }
+ 
 }]);
-
-
-
-
-
